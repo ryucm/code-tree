b8cbba863e0662e57e3f9fc78f33e77d73ca57e3
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-16 22:47:40 +0900

    Title: Date to Date, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/Date to Date/date-to-date.py
+++ 241016/Date to Date/date-to-date.py
@@ -1,7 +1,4 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
 calender = [31, 28, 31, 30, 31, 30 ,31, 31, 30, 31, 30, 31]
-if m1 == m2:
-    print((sum(calender[:m2]) + d2) - (sum(calender[:m1]) +d1)+ 1)
-else:
-    print((sum(calender[:m2]) + d2) - (sum(calender[:m1]) +d1))
+print((sum(calender[:m2-1]) + d2) - (sum(calender[:m1-1]) +d1) +1)
