a348620aa2c2cf42a4f87ef66e7fbdd31fbbfdb5
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-17 23:08:29 +0900

    Title: 2개씩 그룹짓기, Time: 109ms, Memory: 25MB, Status: Passed - CodeTree

--- 241017/2개씩 그룹짓기/group-of-pairs.py
+++ 241017/2개씩 그룹짓기/group-of-pairs.py
@@ -2,4 +2,12 @@ N = int(input())
 
 arr = sorted(list(map(int,input().split())), reverse=True)
 
-print(arr[(len(arr) - (len(arr) // 2))] + arr[(len(arr) - (len(arr) // 2)) -1])
+group_max = 0
+for i in range(N):
+    # i번째와 2n - 1 - i번째 원소를 매칭합니다.
+    group_sum = arr[i] + arr[2*N - 1 - i]
+    if group_sum > group_max:
+        # 최댓값을 갱신합니다.
+        group_max = group_sum
+
+print(group_max)
