f961f47313c6c9d0a4a1bfeaa966fe391bbcee95
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-17 11:59:37 +0900

    Title: 그 요일은, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,8 @@
+calender = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+week_list = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+week = input()
+
+diff = sum(calender[:m2]) + d2 - sum(calender[:m1]) - d1
+print(diff // 7 + 1 if diff % 7 <= 5 else diff // 7)
