095e2de4778ce038be628c0de128ed390d6e79d8
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-17 23:30:20 +0900

    Title: 중앙값 계산 2, Time: 98ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/중앙값 계산 2/get-median-2.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+result = []
+
+for i in range(1, n+1):
+    if i % 2 != 0:
+        result.append(arr[:i][(i-1) // 2])
+print(" ".join(map(str,result)))
