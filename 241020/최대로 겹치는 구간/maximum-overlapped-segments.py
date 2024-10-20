c1a0ece540324336a61e08a3ab84955d277790ca
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-20 13:46:27 +0900

    Title: 최대로 겹치는 구간, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 241020/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ 241020/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,14 +1,9 @@
 n = int(input())
-arr = [0] * 100
+arr = [0] * 200
 for i in range(n):
     x1, x2 = tuple(map(int, input().split()))
-    if x1 < 0:
-        x1 += -x1
-        x2 = -x1 + x2
-    elif x2 < 0:
-        x1 = -x2 + x1
-        x2 += -x2
-    for i in range(x1, x2):
+    
+    for i in range(x1 + 100, x2 + 100):
         arr[i] += 1
 
 print(max(arr))
