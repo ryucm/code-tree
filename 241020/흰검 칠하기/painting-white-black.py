cc9703f60fb6c322d4b216d9796537894627d4ce
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-20 15:00:33 +0900

    Title: 흰검 칠하기, Time: 120ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 241020/흰검 칠하기/painting-white-black.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+arr = [[0, 0, 0]] * (2000 * 100 + 1)
+idx = 1000 * 100
+
+for i in range(n):
+    x, o = input().split()
+    x = int(x)
+
+    if o == 'L':
+        for j in range(idx-x, idx):
+            arr[j] = [arr[j][0] + 1, arr[j][1], -1]
+        idx -= x
+    
+    if o == 'R':
+        for j in range(idx, idx+x):
+            arr[j] = [arr[j][0], arr[j][1] + 1, 1]
+        idx += x
+
+white = black = gray = 0
+for i in arr:
+    if i[0] >= 2 and i[1] >= 2:
+        gray += 1
+    elif i[2] == -1:
+        white += 1
+    elif i[2] == 1:
+        black += 1
+
+print(white, black, gray)
