6d0dfe9eb608b6d575b140e770385a8e80b95bc5
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-20 15:19:09 +0900

    Title: 흰검 칠하기, Time: 135ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241020/흰검 칠하기/painting-white-black.py
+++ 241020/흰검 칠하기/painting-white-black.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-arr = [[0, 0, 0]] * (2000 * 100 + 1)
+arr = [[0, 0, '']] * (2000 * 100 + 1)
 idx = 1000 * 100
 
 for i in range(n):
@@ -8,22 +8,22 @@ for i in range(n):
     x = int(x)
 
     if o == 'L':
-        for j in range(idx-x, idx):
-            arr[j] = [arr[j][0] + 1, arr[j][1], -1]
-        idx -= x
+        for j in range(idx-x+1, idx+1):
+            arr[j] = [arr[j][0] + 1, arr[j][1], 'W']
+            idx = j
     
     if o == 'R':
         for j in range(idx, idx+x):
-            arr[j] = [arr[j][0], arr[j][1] + 1, 1]
-        idx += x
+            arr[j] = [arr[j][0], arr[j][1] + 1, 'B']
+            idx = j
 
 white = black = gray = 0
 for i in arr:
     if i[0] >= 2 and i[1] >= 2:
         gray += 1
-    elif i[2] == -1:
+    elif i[2] == 'W':
         white += 1
-    elif i[2] == 1:
+    elif i[2] == 'B':
         black += 1
 
 print(white, black, gray)
