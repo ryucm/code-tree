1651cad88572b031ce276935fb08a5278ba9e829
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-20 13:13:25 +0900

    Title: 블럭쌓는 명령2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241020/블럭쌓는 명령2/block-stacking-commands2.py
+++ 241020/블럭쌓는 명령2/block-stacking-commands2.py
@@ -3,7 +3,7 @@ result = [0]*N
 
 for i in range(K):
     A, B = tuple(map(int, input().split()))
-    for j in range(A, B+1):
+    for j in range(A-1, B):
         result[j] += 1
 
 print(max(result))
