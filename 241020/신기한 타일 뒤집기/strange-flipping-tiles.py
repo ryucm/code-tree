4b29bbd9d8d59f1fb36f276aedd4b2e6fe9f3e27
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-20 15:44:41 +0900

    Title: 신기한 타일 뒤집기, Time: 144ms, Memory: 27MB, Status: Wrong Answer - CodeTree

---
+++ 241020/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,33 @@
+n = int(input())
+
+arr = [0] * (2 * 1000 * 100 + 1)
+idx = 1000 * 100
+
+for i in range(n):
+    x, o = input().split()
+    x = int(x)
+
+    if o == 'L':
+        for j in range(idx-x+1, idx+1):
+            # print(j, 'W')
+            arr[j] = -1
+        idx -= x
+        # print(idx)
+    
+    else:
+        for j in range(idx, idx+x):
+            # print(j, 'B')
+            arr[j] = 1
+        idx += x -1
+        # print(idx)
+
+white = 0
+black = 0
+
+for i in arr:
+    if i == -1:
+        white += 1
+    elif i == 1:
+        black += 1
+
+print(white, black)
