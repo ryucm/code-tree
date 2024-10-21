0330b4870e467b92ef8fabc57a550ff894ed1ceb
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-22 02:16:52 +0900

    Title: 연속되는 수 2, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 241022/연속되는 수 2/continuous-number2.py
+++ 241022/연속되는 수 2/continuous-number2.py
@@ -3,13 +3,15 @@ arr = []
 for i in range(N):
     arr.append(int(input()))
 
-max_x = 0
+max_x = 1
 cnt = 1
-for i in range(1, len(arr)):
-    if arr[i] == arr[i-1]:
+for i in range(len(arr)):
+    if i == 0:
+        continue
+    elif arr[i] == arr[i-1]:
         cnt += 1
-        max_x = max_x if cnt < max_x else cnt
     else:
         cnt = 1
+    max_x = max_x if cnt < max_x else cnt
 
 print(max_x)
