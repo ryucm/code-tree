2222471a8a0a92daab3e1f7ca9c4e7c86cc9d5f7
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-22 14:00:33 +0900

    Title: 선두를 지켜라, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241022/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,42 @@
+N, M = tuple(map(int, input().split()))
+arr_A = [0] * 1001
+arr_B = [0] * 1001
+cur_A = 0
+cur_B = 0
+
+for i in range(N):
+    v, t = tuple(map(int, input().split()))
+
+    for i in range(cur_A+1, t+cur_A+1):
+        arr_A[i] = arr_A[i-1] + v
+        cur_A += 1
+for i in range(M):
+    v, t = tuple(map(int, input().split()))
+
+    for i in range(cur_B+1, t+cur_B+1):
+        arr_B[i] = arr_B[i-1] + v
+        cur_B += 1
+
+cnt = 0
+previous_value = ''
+for i in range(1001):
+    temp = ''
+    if previous_value == '':
+        previous_value = 'A' if arr_A[i] > arr_B[i] else 'B'
+        continue
+    
+    if arr_A[i] + arr_A[i-1] > arr_B[i] + arr_B[i-1]:
+        if previous_value == 'A':
+            continue
+        else:
+            previous_value = 'A'
+            cnt += 1
+    elif arr_A[i] + arr_A[i-1] < arr_B[i] + arr_B[i-1]:
+        if previous_value == 'B':
+            continue
+        else:
+            previous_value = 'B'
+            cnt += 1
+            
+    
+print(cnt)
