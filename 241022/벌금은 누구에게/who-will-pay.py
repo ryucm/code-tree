7a99087b52e8211b4895db7e8c5fe1eecdcd83c5
류창민 <89899249+ryucm@users.noreply.github.com>
2024-10-22 11:15:23 +0900

    Title: 벌금은 누구에게, Time: 217ms, Memory: 26MB, Status: Passed - CodeTree

--- 241022/벌금은 누구에게/who-will-pay.py
+++ 241022/벌금은 누구에게/who-will-pay.py
@@ -1,11 +1,12 @@
 N, M, K = tuple(map(int, input().split()))
 students = list(range(1, N+1))
+f = [0]*len(students)
 
 result = -1
 for i in range(1, M+1):
     num = int(input())
 
-    if num in students and result == -1:
+    f[num-1] += 1
+    if f[num-1] >= K and result == -1:
         result = num
-
 print(result)
